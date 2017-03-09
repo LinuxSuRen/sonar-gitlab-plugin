4c8473967731abe2955a6d2054cb172b3f09c105
Gabriel Allaigre <gabriel.allaigre@talanlabs.com>
2017-03-09 12:22:15 +0100

    fail

---
+++ src/main/java/com/talanlabs/sonar/plugins/gitlab/Fake.java
@@ -0,0 +1,19 @@
+package com.talanlabs.sonar.plugins.gitlab;
+
+import java.util.List;
+
+public class Fake {
+
+    List<String> ss;
+
+    public Fake(List<String> ss) {
+        this.ss = ss;
+    }
+
+    public void fonction() {
+        String toto = null;
+        System.out.println(toto.length());
+    }
+
+
+}
